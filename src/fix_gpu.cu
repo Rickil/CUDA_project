12a951b4d54057a4b86a7b2988b23e07a76102b2
yanis.farhat <farhat.yanis@yahoo.fr>
2023-11-09 10:42:35 +0100

    commented checks

--- src/fix_gpu.cu
+++ src/fix_gpu.cu
@@ -147,24 +147,24 @@ void compact_image_gpu(int* d_buffer, int* d_output, int image_size, int compact
     int gridSize = (image_size + blockSize - 1) / blockSize;
 
     //copy the predicate for further tests
-    int *d_buffer_copy;
+    /*int *d_buffer_copy;
     cudaMalloc(&d_buffer_copy, image_size*sizeof(int));
-    cudaMemcpy(d_buffer_copy, d_buffer, image_size*sizeof(int), cudaMemcpyDeviceToDevice);
+    cudaMemcpy(d_buffer_copy, d_buffer, image_size*sizeof(int), cudaMemcpyDeviceToDevice);*/
 
     predicate_kernel<<<gridSize, blockSize>>>(d_buffer, d_predicate, image_size);
-    check_predicate(d_buffer_copy, d_predicate, image_size);
+    //check_predicate(d_buffer_copy, d_predicate, image_size);
 
-    int *d_predicate_copy;
+    /*int *d_predicate_copy;
     cudaMalloc(&d_predicate_copy, image_size*sizeof(int));
-    cudaMemcpy(d_predicate_copy, d_predicate, image_size*sizeof(int), cudaMemcpyDeviceToDevice);
+    cudaMemcpy(d_predicate_copy, d_predicate, image_size*sizeof(int), cudaMemcpyDeviceToDevice);*/
 
     //exclusive_scan(d_predicate, image_size);
     decoupled_lookback(d_predicate, image_size);
-    check_scan(d_predicate_copy, d_predicate, image_size);
+    //check_scan(d_predicate_copy, d_predicate, image_size);
 
     cudaMemcpy(d_output, d_buffer, image_size*sizeof(int), cudaMemcpyDeviceToDevice);
     scatter_kernel<<<gridSize, blockSize>>>(d_buffer, d_predicate, d_output, image_size);
-    check_scatter(d_output, d_buffer_copy, d_predicate, image_size, compact_size);
+    //check_scatter(d_output, d_buffer_copy, d_predicate, image_size, compact_size);
 
     cudaFree(d_predicate);
 }
@@ -331,14 +331,14 @@ void fix_image_gpu(Image& image){
     cudaMemcpy(image.buffer, d_output, image.width*image.height * sizeof(int), cudaMemcpyDeviceToHost);
 
     //check image_buffer
-    int count = 0;
+    /*int count = 0;
     for (int i=0; i<compact_size; i++){
         if (image.buffer[i] == -27){
             count++;
             //printf("value -27 at %d\n", i);
         }
     }
-    printf("%d values at -27\n", count);
+    printf("%d values at -27\n", count);*/
 
     cudaFree(d_buffer);
     cudaFree(d_output);
